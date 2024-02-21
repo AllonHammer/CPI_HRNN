3a0b41afc8d38668ccb7b81e216804e333a664d9
eliyacohen1 <42353946+eliyacohen1@users.noreply.github.com>
2024-02-21 13:23:06 +0200

    Update configs.py

--- config/configs.py
+++ config/configs.py
@@ -14,7 +14,7 @@ window = 0.3
 MODEL_NAME = "HRNN 4"
 TRAIN_NAME = f'{MODEL_NAME}_lr_{h_gru_params["lr"]}_hidden_{h_gru_params["hidden_gru_units"]}_batch_{h_gru_params["batch_size"]}_patience_{h_gru_params["patience"]}'
 
-MODEL_DATA_PATH_CPI = "data/cpi/cpi_us_dataset_s.csv"
+MODEL_DATA_PATH_CPI = "data/cpi/cpi_us_dataset.csv"
 MODELS_TORCH = 'torch_models'
 BASELINES_MODELS = False
 LOAD_DATA = True
